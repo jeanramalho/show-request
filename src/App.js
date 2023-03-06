7f13c4a57fe4a81c0b98f3f57f24d4df978d5056
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-06 14:02:25 -0300

    Falta um erro para começar a compilar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GlobalStyle from "./Styles/global";
-import Router from "./components/Router";
+import Router from "./Components/Router";
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
@@ -8,12 +8,12 @@ function App() {
   return (
     
 
-     <ApolloProvider client={client}>
+
         <BrowserRouter>
         <Router />   
         <GlobalStyle />  
        </BrowserRouter>          
-    </ ApolloProvider>
+
     
         
   
