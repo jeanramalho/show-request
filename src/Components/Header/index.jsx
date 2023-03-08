0a855d2d224a29288e198ddb62c25cdf76c8e547
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-07 21:18:04 -0300

    Insere classe na logo

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <Container>
       <Logo />
-      <Link to="/" className='link'><h1><Span>Show</Span> Request</h1></Link>
+      <Link to="/" className='link'><h1 className='title-header'><span className='show-title'>Show</span> Request</h1></Link>
     </Container>
   )
 }
