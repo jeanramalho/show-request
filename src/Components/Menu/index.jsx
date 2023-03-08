e05050da53e288626f7a75ae446ea5da5582738c
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-07 21:34:23 -0300

    Inserindo itens no menu

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -6,7 +6,12 @@ import { Link } from 'react-router-dom';
 function Menu() {
   return (
     <Container>
-
+        <ul className='lista-menu'>
+            <Link className='item-menu'><ul>Onsite</ul></Link>
+            <Link className='item-menu'><ul>Busca</ul></Link>
+            <Link className='item-menu'><ul>Navigates</ul></Link>
+            <Link className='item-menu'><ul>Hotsite</ul></Link>
+        </ul>
     </Container>
   );
 }
