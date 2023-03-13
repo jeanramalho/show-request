687b5b7a3525aceffcf5255a7e45d8d8b39b824d
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-13 16:40:38 -0300

    Cria classe para estilizar menu header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       <Logo />
       <Link to="/" className='link'>
         <h1>Show Request</h1>
-        <ul>
+        <ul className='menuHeader'>
           <Link><li>Onsite</li></Link>
           <Link><li>Search</li></Link>
           <Link><li>Hotsite</li></Link>
