917fbe301572c1dbc9389a4fb83e59e63f0c0cc1
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-13 16:43:44 -0300

    Alinha conteudos no header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -9,13 +9,14 @@ export default function Header() {
       <Logo />
       <Link to="/" className='link'>
         <h1>Show Request</h1>
+        </Link>
+
         <ul className='menuHeader'>
           <Link><li>Onsite</li></Link>
           <Link><li>Search</li></Link>
           <Link><li>Hotsite</li></Link>
           <Link><li>Navigates</li></Link>
         </ul>
-        </Link>
     </Container>
   )
 }
