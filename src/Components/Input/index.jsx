f6890cab0caf5143c5772dc99aa6b2f4c07445c6
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-13 16:30:01 -0300

    Cria componente de input

---
+++ src/Components/Input/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './style'
+import { Link } from 'react-router-dom';
+import Logo from '../Logo'
+
+export default function Header() {
+  return (
+    <Container>
+      <Logo />
+      <Link to="/" className='link'><h1>Show Request</h1></Link>
+    </Container>
+  )
+}
---
+++ src/Components/Input/style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 4rem;
+    padding: 0 30px;
+    background: #d65a1c;
+    color: #FFF;
+    display: flex;
+    align-items: center;
+    text-decoration: none !important;
+
+    .link {
+        text-decoration: none !important;
+        color: #fff;
+       }
+
+`;
